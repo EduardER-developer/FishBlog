fc243ea78b47d71b19d1ca354cd61c1505bdc364
EduardER-developer <frkd10wot@yandex.ru>
2025-01-17 14:23:20 +0300

    first commit

---
+++ components/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = 'root';
+$password = '';
+$db = 'localhost';
+$name = 'fishblog';
+
+$link = new mysqli($db, $user, $password, $name);
+
+if ($link->connect_error) {
+    die('Ошибка подключения (' . $link->connect_errno . ') '
+     . $link->connect_error);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// подключение к бд
+include_once 'components/config.php';
+// подключение контроллера
+include_once 'Controllers/'
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
